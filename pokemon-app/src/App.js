319fd3effea03ba88e76f8b99d672d921bcf6fdf
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-04-13 16:11:12 -0700

    finished project

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [currentPokemon, setCurrentPokemon] = useState(null)
+
+  useEffect(() => {
+    const fetchPoke = async () => {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon/lucario')
+      setCurrentPokemon(await res.json())
+    }
+
+    fetchPoke()
+  }, [])
+
+  console.log('Current pokemon=>', currentPokemon)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon App</h1>
+      <h3>This is my current favorite Pokemon: </h3>
+      <br />
+
+      {currentPokemon ? 
+        <>
+        <div>
+          <h2>{currentPokemon.name}</h2>
+          <img src={currentPokemon.sprites.front_default} alt={currentPokemon.name} />
+        </div>
+        <div>
+          <ul>
+            Moves: 
+            {currentPokemon.moves.slice(0,5).map((move, index) => {
+              return <li key={index}>{move.move.name}</li>
+            })}
+          </ul>
+        </div>
+        </>
+        :
+
+        <div>No Pokemon selected yet</div>
+      }
     </div>
+    
   );
 }
 
